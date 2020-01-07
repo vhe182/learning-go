19e729d9cd727a042f5305a577d46efdbc0c293e
Victor Hugo Estrada <vhe182@gmail.com>
2020-01-06 21:11:48 -0500

    Read and print all mass module values from file
    Figured out how to use the os.Open and os.Read functions.

--- src/main/hello.go
+++ src/main/hello.go
@@ -2,18 +2,37 @@ package main
 
 import (
 	"fmt"
-	//"os"
+	"os"
 	"puzzleutil"
 	"strconv"
 	"stringutil"
 )
 
+func PrintHelp(cmd string) {
+	fmt.Fprintf(os.Stderr, "This tool is used to calculate and demonstrate solutions "+
+		"to the 2019 advent of code challenges\n")
+	fmt.Fprintf(os.Stderr, "Usage: %s <path/to/input-file>\n", os.Args[0])
+}
+
 func main() {
-	s := "hello, world\n"
-	//file, openError := os.Open("resources/input.txt")
-	//var line []byte
-	//numRead, readError := os.Read(line)
-	fmt.Printf(s)
-	fmt.Printf(strconv.Itoa(puzzleutil.CalculateFuel(10)))
-	fmt.Printf(stringutil.Reverse(s))
+	clArgs := os.Args[1:]
+	if len(clArgs) != 1 {
+		PrintHelp(os.Args[0])
+		return
+	}
+	_, err := os.Open(clArgs[0])
+	if err != nil {
+		fmt.Printf("Failed to open resource file.\n")
+		return
+	}
+	fmt.Printf(clArgs[0])
+	fmt.Println()
+	if puzzleutil.ReadMassModuleFile(clArgs[0]) < 0 {
+		fmt.Println("Error: Failed to read in mass module file")
+		return
+	}
+	fmt.Println()
+	fmt.Println(strconv.Itoa(puzzleutil.CalculateFuel(10)))
+	fmt.Println()
+	fmt.Printf(stringutil.Reverse(clArgs[0]))
 }
--- src/puzzleutil/puzzleutil.go
+++ src/puzzleutil/puzzleutil.go
@@ -1,8 +1,37 @@
 // Package for solving advent of code puzzle 1
 package puzzleutil
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+// brief, Open module mass file and read in all component masses
+// param filename, A full or relative path to a mass module file
+func ReadMassModuleFile(filename string) int {
+	fmt.Println("In ReadMassModuleFile\n")
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println(err)
+		return -1
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error occurred while reading in strings from file: " + filename)
+	}
+
+	return 10
+}
+
 // brief, Given a value this function calculates expected fuel cost
-// param val, a unit to use for calculating fuel cost
+// param val, A unit to use for calculating fuel cost
 func CalculateFuel(val int) int {
 	return val
 }
